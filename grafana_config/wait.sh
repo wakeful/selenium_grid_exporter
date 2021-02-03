c59aca6d5a606184cab5c2ffbaf1facc187d9ec8
Martin Copjan <martin.copjan@derivco.co.uk>
2021-02-03 10:25:07 +0000

    add prometheus and grafana

--- grafana_config/wait.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-while ! curl --output /dev/null --silent --head --fail http://grafana:3000; do sleep 1 && echo -n .; done;
-
-curl -v -H "Content-Type: application/json" -X POST -d @prometheus_datasource.json http://grafana:3000/api/datasources
-
-curl -v -H "Content-Type: application/json" -X POST -d @selenium_grid_dashboard.json http://grafana:3000/api/dashboards/db
